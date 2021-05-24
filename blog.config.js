343a0b8fa812714bbec7db6a66e32168b6388a92
Lcwhhh <56891457+Lcwhhh@users.noreply.github.com>
2021-05-24 14:32:48 -0700

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -12,11 +12,11 @@ const BLOG = {
   path: '', // leave this empty unless you want to deploy Nobelium in a folder
   since: 2021, // if leave this empty, current year will be used.
   postsPerPage: 7,
-  sortByDate: false,
+  sortByDate: true,
   showAbout: true, // WIP
   showArchive: true, // WIP
   autoCollapsedNavBar: false, // the automatically collapsed navigation bar
-  socialLink: 'https://twitter.com/craigaryhart',
+  socialLink: 'https://twitter.com/Lcwhhh',
   seo: {
     keywords: ['Blog', 'Website', 'Notion'],
     googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
